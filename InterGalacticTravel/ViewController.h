2e58e245005584a424ce5c72bfc9563a215113f1
diegocichello <diegocichello@Diegos-MacBook-Pro.local>
2015-01-06 10:56:37 -0800

    Initial Commit

---
+++ InterGalacticTravel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InterGalacticTravel
+//
+//  Created by Diego Cichello on 1/6/15.
+//  Copyright (c) 2015 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
